ca0fddcc49be0e2e614973733ab4672093a7f308
Joel <joelangeltw@gmail.com>
2021-09-29 13:13:18 -0500

    fisrt commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get '/bienvenida', to: 'home#index'
+  
+  root to: 'home#index'
+
+  get 'articles' , to:'articles#index'
+
+  get 'articles/new', to: 'articles#new', as: :new_articles
+  get 'articles/:id', to: 'articles#show'
+  get 'articles/:id/edit', to: 'articles#edit'
+
+  patch '/articles/:id', to: 'articles#update', as: :article
+  post 'articles', to: 'articles#create'
+  delete '/articles/:id', to: 'articles#destroy'
+end
